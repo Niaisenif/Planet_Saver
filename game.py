51f126401c1c5670ffed668efb03fe3595a75422
Niaisenif <lohacharubi@gmail.com>
2022-05-08 20:46:54 +0200

    left and right

--- game.py
+++ game.py
@@ -9,13 +9,16 @@ class Game:
         self.pressed = {}
         self.player = Player(self)
         self.asteroid = Asteroid()
-        print(math.sqrt((self.asteroid.rect.centerx + self.player.rect.centerx)**2 + (self.asteroid.rect.centery + self.player.rect.centery)**2))
+        self.font = pygame.font.SysFont('monospace', 50)
 
     def update(self):
-        self.player.gravity()
+        score_text = self.font.render(f'Distance : {self.get_distance_p_a()}', 1, (255, 255, 255))
+        self.screen.blit(score_text, (20, 20))
+
+        self.player.angle()
         self.player.fix_coordinates()
         self.screen.blit(self.player.image, self.player.rect)
         self.screen.blit(self.asteroid.image, self.asteroid.rect)
 
-    def distance_p_a(self):
-        return math.sqrt((self.asteroid.rect.centerx + self.player.rect.centerx)**2 + (self.asteroid.rect.centery + self.player.rect.centery)**2)
+    def get_distance_p_a(self):
+        return math.sqrt(((self.asteroid.rect.centerx - self.player.real_center[0])**2) + ((self.asteroid.rect.centery - self.player.real_center[1])**2))
--- player.py
+++ player.py
@@ -19,39 +19,63 @@ class Player(pygame.sprite.Sprite):
         self.relative_up = [0, -1]
         self.relative_down = [0, 1]
 
-
-    def gravity(self):
+    def angle(self):
         self.angle = math.degrees(math.atan2(- self.rect.centery + self.game.asteroid.rect.centery,
                                              self.rect.centerx - self.game.asteroid.rect.centerx)) - 90
         self.image = pygame.transform.rotozoom(self.origin_image, self.angle, 1)
         self.rect = self.image.get_rect(center=self.rect.center)
 
-        self.relative_up = pygame.Vector2(self.real_center[0] - self.game.asteroid.rect.centerx,
-                                          self.real_center[1] - self.game.asteroid.rect.centery).normalize()
+        # Avoid error when player is behind asteroid
+        # Exception has occurred: ValueError       (note: full exception trace is shown but execution is paused at: _run_module_as_main)
+        # Can't normalize Vector of length Zero
+        #
+        if (self.real_center[0] - self.game.asteroid.rect.centerx) and (
+                self.real_center[1] - self.game.asteroid.rect.centery
+        ):
+            self.relative_up = pygame.Vector2(
+                self.real_center[0] - self.game.asteroid.rect.centerx,
+                self.real_center[1] - self.game.asteroid.rect.centery,
+            ).normalize()
 
-        self.relative_down = pygame.Vector2(self.game.asteroid.rect.centerx - self.real_center[0],
-                                            self.game.asteroid.rect.centery - self.real_center[1]).normalize()
+            self.relative_down = pygame.Vector2(
+                self.game.asteroid.rect.centerx - self.real_center[0],
+                self.game.asteroid.rect.centery - self.real_center[1],
+            ).normalize()
 
-        self.relative_right[0], self.relative_right[1] = self.relative_down[1], self.relative_down[0]
+            c = math.cos(math.pi * (self.angle + 90) / 180)
+            s = math.sin(math.pi * (self.angle + 90) / 180)
+            self.relative_right = [s, c]
+            self.relative_left = [-s, -c]
 
-        self.relative_left[0], self.relative_left[1] = self.relative_up[1], self.relative_up[0]
-
+        if 111 > self.game.get_distance_p_a() > 110:
+            while self.game.get_distance_p_a() > 110:
+                self.real_center[0], self.real_center[1] = (self.real_center[0] + 0.01 * self.relative_down[0]), (
+                        self.real_center[1] + 0.01 * self.relative_down[1])
+        if 130 > self.game.get_distance_p_a() > 111:
+            i = 0
+            while self.game.get_distance_p_a() > 111 and i < 10:
+                self.real_center[0], self.real_center[1] = (self.real_center[0] + 1 * self.relative_down[0]), (
+                        self.real_center[1] + 1 * self.relative_down[1])
+                i += 1
 
     def move_left(self):
         self.real_center[0], self.real_center[1] = (self.real_center[0] + 10 * self.relative_left[0]), (
-                    self.real_center[1] + 10 * self.relative_left[1])
+                self.real_center[1] + 10 * self.relative_left[1])
 
     def move_right(self):
         self.real_center[0], self.real_center[1] = (self.real_center[0] + 10 * self.relative_right[0]), (
-                    self.real_center[1] + 10 * self.relative_right[1])
+                self.real_center[1] + 10 * self.relative_right[1])
 
     def move_down(self):
         self.real_center[0], self.real_center[1] = (self.real_center[0] + 10 * self.relative_down[0]), (
-                    self.real_center[1] + 10 * self.relative_down[1])
+                self.real_center[1] + 10 * self.relative_down[1])
 
     def move_up(self):
         self.real_center[0], self.real_center[1] = (self.real_center[0] + 10 * self.relative_up[0]), (
-                    self.real_center[1] + 10 * self.relative_up[1])
+                self.real_center[1] + 10 * self.relative_up[1])
 
     def fix_coordinates(self):
         self.rect.centerx, self.rect.centery = round(self.real_center[0]), round(self.real_center[1])
+
+
+
